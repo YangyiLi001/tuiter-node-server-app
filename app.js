6021f6c026a705653137bfc01ba906500e97a82e
“YangyiLi001” <yangyili1994@gmail.com>
2022-11-30 17:46:32 -0800

    a9 fall2022v2

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 
 // const express = require('express')
-const DB_CONNECTION_STRING = "mongodb+srv://yangyili1994:supersecretpassword@cluster0.jcgn0n3.mongodb.net/?retryWrites=true&w=majority"
+// const DB_CONNECTION_STRING = "mongodb+srv://yangyili1994:supersecretpassword@cluster0.4zgikds.mongodb.net/tuiter?retryWrites=true&w=majority"
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors())
